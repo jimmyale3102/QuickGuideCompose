0cb30fec3abd2ce9c00da2326f03a80d181d9603
Jimmyale3102 <jimmyale3102@gmail.com>
2024-09-22 19:55:02 +1000

    feat: Add Medium top app bar

--- app/src/main/java/dev/alejo/quickguidecompose/MainActivity.kt
+++ app/src/main/java/dev/alejo/quickguidecompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import dev.alejo.quickguidecompose.composables.MediumTopAppBarExample
 import dev.alejo.quickguidecompose.ui.theme.QuickGuideComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QuickGuideComposeTheme {
-                
+                MediumTopAppBarExample()
             }
         }
     }
