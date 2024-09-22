b59faabcdcc613981003ccb76724f256b0885f24
Jimmyale3102 <jimmyale3102@gmail.com>
2024-09-22 19:34:33 +1000

    feat: Add Centered Aligned top app bar

---
+++ app/src/main/java/dev/alejo/quickguidecompose/composables/CenteredAlignedTopAppBar.kt
@@ -0,0 +1,55 @@
+package dev.alejo.quickguidecompose.composables
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CenteredAlignedTopAppBarExample() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = { Text("Small TopAppBar") },
+                navigationIcon = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        ScrollContent(innerPadding = innerPadding)
+    }
+}
+
+@Preview
+@Composable
+fun CenteredAlignedTopAppBarPreview() {
+    CenteredAlignedTopAppBarExample()
+}
