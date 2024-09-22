f2d2dc40950843c7091ff2279b8ac16241611aff
Jimmyale3102 <jimmyale3102@gmail.com>
2024-09-22 18:58:28 +1000

    chore: Fix conflicts

---
+++ app/src/main/java/dev/alejo/quickguidecompose/MainActivity.kt
@@ -0,0 +1,19 @@
+package dev.alejo.quickguidecompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import dev.alejo.quickguidecompose.ui.theme.QuickGuideComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuickGuideComposeTheme {
+                
+            }
+        }
+    }
+}
