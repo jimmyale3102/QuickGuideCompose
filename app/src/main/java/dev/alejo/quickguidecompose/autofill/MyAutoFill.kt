2cc4b090d0c7893dda4316600313604ea08f193d
Jimmyale3102 <jimmyale3102@gmail.com>
2025-04-25 15:56:15 +0700

    feat: Update Autofill UI

--- app/src/main/java/dev/alejo/quickguidecompose/autofill/MyAutoFill.kt
+++ app/src/main/java/dev/alejo/quickguidecompose/autofill/MyAutoFill.kt
@@ -2,7 +2,6 @@ package dev.alejo.quickguidecompose.autofill
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
